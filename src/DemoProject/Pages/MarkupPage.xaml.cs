8f87591a95351b11e353c0dfa9b2e75b6f3a7e17
Matt Goldman <matt.goldman@outlook.com>
2023-01-14 13:19:39 +1100

    Initial markup extension working

---
+++ src/DemoProject/Pages/MarkupPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DemoProject.Pages;
+
+public partial class MarkupPage : ContentPage
+{
+	public MarkupPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Maui.Plugins.PageResolver/MarkupExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Controls.Xaml;
+using System;
+
+namespace Maui.Plugins.PageResolver;
+
+public class ResolveViewModel<T> : IMarkupExtension<T>
+{
+    public string MyProperty { get; set; }
+
+    public T ProvideValue(IServiceProvider serviceProvider)
+    {
+        var sp = Resolver.GetServiceProvider();
+        var result = sp.GetRequiredService<T>();
+
+        return result;
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return ProvideValue(serviceProvider);
+    }
+}
