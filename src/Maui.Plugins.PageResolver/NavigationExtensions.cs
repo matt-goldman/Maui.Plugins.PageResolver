355428282f0401151d3d8e4803a8d711db6f0f61
Matt Goldman <matt.goldman@outlook.com>
2023-10-08 12:24:36 +1100

    Working with page params + viewmodel

--- src/Maui.Plugins.PageResolver/NavigationExtensions.cs
+++ src/Maui.Plugins.PageResolver/NavigationExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Maui.Controls;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maui.Plugins.PageResolver;
@@ -91,18 +92,76 @@ public static class NavigationExtensions
 
     private static Page CreatePageWithViewModel<T>(IServiceProvider serviceProvider, Type viewModelType, params object[] parameters) where T : Page
     {
-        var viewModel = ActivatorUtilities.CreateInstance(serviceProvider, viewModelType, parameters);
+        // Check if parameters fit the ViewModel's constructors
+        if (ParametersMatchConstructors(viewModelType, parameters))
+        {
+            var viewModel = ActivatorUtilities.CreateInstance(serviceProvider, viewModelType, parameters);
+            return CreatePageUsingViewModel<T>(serviceProvider, viewModel);
+        }
+        // Check if parameters fit the Page's constructors, excluding the ViewModel type
+        else if (ParametersMatchConstructorsExcludingType(typeof(T), viewModelType, parameters))
+        {
+            return ActivatorUtilities.CreateInstance<T>(serviceProvider, parameters);
+        }
+        else
+        {
+            throw new ArgumentException("Provided parameters do not match the constructors of the Page or ViewModel.");
+        }
+    }
 
+    private static bool ParametersMatchConstructors(Type type, params object[] parameters)
+    {
+        var constructors = type.GetConstructors();
+        foreach (var constructor in constructors)
+        {
+            var ctorParams = constructor.GetParameters();
+            if (ctorParams.Length == parameters.Length)
+            {
+                for (int i = 0; i < ctorParams.Length; i++)
+                {
+                    if (!ctorParams[i].ParameterType.IsAssignableFrom(parameters[i].GetType()))
+                        break;
+                    if (i == ctorParams.Length - 1)
+                        return true; // all parameters match
+                }
+            }
+        }
+        return false;
+    }
+
+    private static bool ParametersMatchConstructorsExcludingType(Type type, Type excludeType, params object[] parameters)
+    {
+        var constructors = type.GetConstructors();
+        foreach (var constructor in constructors)
+        {
+            var ctorParams = constructor.GetParameters().Where(p => p.ParameterType != excludeType).ToArray();
+            if (ctorParams.Length == parameters.Length)
+            {
+                for (int i = 0; i < ctorParams.Length; i++)
+                {
+                    if (!ctorParams[i].ParameterType.IsAssignableFrom(parameters[i].GetType()))
+                        break;
+                    if (i == ctorParams.Length - 1)
+                        return true; // all parameters match
+                }
+            }
+        }
+        return false;
+    }
+
+    private static Page CreatePageUsingViewModel<T>(IServiceProvider serviceProvider, object viewModel) where T : Page
+    {
         try
         {
             return ActivatorUtilities.CreateInstance<T>(serviceProvider, viewModel);
-
         }
         catch (MissingMemberException)
         {
-            return ActivatorUtilities.CreateInstance<T>(Resolver.GetServiceProvider(), parameters);
+            return ActivatorUtilities.CreateInstance<T>(Resolver.GetServiceProvider());
         }
     }
-    
+
+
+
     #endregion
 }
