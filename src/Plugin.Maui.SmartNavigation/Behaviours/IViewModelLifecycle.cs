f67fca22100470afa65bd55f81305419d5117040
Matt Goldman <matt.goldman@outlook.com>
2025-11-06 09:25:49 +1100

    Closes #57

--- src/Plugin.Maui.SmartNavigation/Behaviours/IViewModelLifecycle.cs
+++ src/Plugin.Maui.SmartNavigation/Behaviours/IViewModelLifecycle.cs
@@ -2,7 +2,19 @@ using System.Threading.Tasks;
 
 namespace Plugin.Maui.SmartNavigation.Behaviours;
 
+/// <summary>
+/// Defines a contract for handling initialization logic in a ViewModel when navigation occurs.
+/// </summary>
+/// <remarks>Implement this interface to perform setup or resource allocation when a ViewModel is first navigated
+/// to or reactivated. Implementations should avoid long-running operations in initialization to ensure responsive navigation.</remarks>
 public interface IViewModelLifecycle
 {
-    Task OnInitAsync();
+    /// <summary>
+    /// Performs asynchronous initialization logic when navigation occurs.
+    /// </summary>
+    /// <param name="isFirstNavigation">Indicates whether this is the first navigation to the component. Pass <see langword="true"/> for the initial
+    /// navigation; otherwise, <see langword="false"/>.</param>
+    /// <returns>A task that represents the asynchronous initialization operation.</returns>
+    /// <remarks>This is called from an async void method in the NavigatedInitBehavior; you MUST handle exceptions in your implementation!</remarks>
+    Task OnInitAsync(bool isFirstNavigation);
 }
--- src/Plugin.Maui.SmartNavigation/Behaviours/NavigatedInitBehavior.cs
+++ src/Plugin.Maui.SmartNavigation/Behaviours/NavigatedInitBehavior.cs
@@ -1,11 +1,12 @@
 #nullable enable
 using Microsoft.Maui.Controls;
+using System;
 
 namespace Plugin.Maui.SmartNavigation.Behaviours;
 
 public class NavigatedInitBehavior : Behavior<Page>
 {
-    private bool _ran;
+    private bool _ran = false;
     
     protected override void OnAttachedTo(Page page)
     {
@@ -15,19 +16,19 @@ public class NavigatedInitBehavior : Behavior<Page>
 
     protected override void OnDetachingFrom(Page page)
     {
+        page.NavigatedTo -= OnNavigatedTo;
         base.OnDetachingFrom(page);
     }
     
     private async void OnNavigatedTo(object? sender, NavigatedToEventArgs e)
     {
-        // TODO: Implement async fire and forget
-        if (_ran) return;
-
-        _ran = true;
-
         if (sender is Page { BindingContext: IViewModelLifecycle viewModel })
         {
-            await viewModel.OnInitAsync();
+            var isFirstNavigation = !_ran;
+
+            await viewModel.OnInitAsync(isFirstNavigation);
+
+            _ran = true;
         }
     }
 }
